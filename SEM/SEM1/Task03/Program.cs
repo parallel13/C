df76b0d1fefcf2fb8095609a4a8b646a1e953346
parallel <kubikrussia@gmai.com>
2022-11-21 22:48:30 +0300

    ввод числа и вывод дня недели

--- SEM/SEM1/Task03/Program.cs
+++ SEM/SEM1/Task03/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая выдает название дня недели по введенному числу
+Console.WriteLine("Введите номер");
+int num1 = Convert.ToInt32(Console.ReadLine());
+switch (num1)
+{
+    
+case 1:
+Console.WriteLine("Понедельник");
+break;
+
+case 2:
+Console.WriteLine("вторник");
+break;
+case 3:
+Console.WriteLine("среда");
+break;
+case 4:
+Console.WriteLine("среда");
+break;
+
+case 5:
+Console.WriteLine("пятница");
+break;
+
+case 6:
+Console.WriteLine("суббота");
+break;
+case 7:
+Console.WriteLine("воскресенье");
+break;
+
+default:
+Console.WriteLine("нет такого дня недели");
+break;
+
+}
