59e1aa7219a2260c76abcb8496452a016c2f1406
parallel <kubikrussia@gmai.com>
2022-11-21 22:18:07 +0300

    является ли число 1 квадратом числа 2

--- SEM/SEM1/Task01/Program.cs
+++ SEM/SEM1/Task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("Введите первое число");
+int sqare = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (sqare == number * number)
+{
+Console.WriteLine($"{sqare} квадрат {number}");
+
+}
+
+
+else Console.WriteLine($"{sqare} не является квадратом {number}");
