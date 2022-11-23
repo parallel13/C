2d032f6b9a0094ca1a3d3714e68154eee7ac37ff
parallel <kubikrussia@gmai.com>
2022-11-23 21:25:30 +0300

    вцикл while

---
+++ SEM/SEM1/Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу которая на вход принимает одно число (N).а в ответе показывает все целые числа в промежутке -N до N
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int counter = -number;
+
+while (counter <= number)
+{
+    Console.Write($"{counter} ");
+    counter++;
+}
