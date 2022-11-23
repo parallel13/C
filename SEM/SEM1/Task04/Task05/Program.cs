60bdcab305ad73a60ddf07a5db9d61dfb70e9487
parallel <kubikrussia@gmai.com>
2022-11-23 21:36:54 +0300

    выводит последнюю цифру введенного трехзначного числа

---
+++ SEM/SEM1/Task04/Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿// написать программу которая на вход принимает трехзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine("введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    System.Console.WriteLine($"Последняя цифра числа => {lastDigit}");
+}
+
